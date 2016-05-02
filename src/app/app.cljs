79177d19f573e14b6dec4162183403a6f6a8cfb9
Ivan Reese <ivanreese@gmail.com>
2016-05-01 23:25:44 -0600

    Remove some needless comments, add a state atom

--- src/app/app.cljs
+++ src/app/app.cljs
@@ -1,9 +1,7 @@
-;;;; App
-;; The app namespace holds most the state, and most of the control flow, in our system.
-;; This file is the entry point of the system, and orchestrates everything.
-
 (ns ^:figwheel-always app.app
   (:require [ajax.core :refer [GET]]))
+
+(defonce state (atom nil))
 
 (defn log [s]
   (.log js/console s))
