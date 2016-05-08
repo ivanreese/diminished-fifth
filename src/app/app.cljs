de4e10a8863c1cb289456b476e8c4b87500f7808
Ivan Reese <ivanreese@gmail.com>
2016-05-07 22:42:03 -0600

    Simpler, trying to get a seq of channels to turn into a vec, hah

--- src/app/app.cljs
+++ src/app/app.cljs
@@ -1,6 +1,5 @@
 (ns ^:figwheel-always app.app
-  (:require [app.manifest :refer [loadManifest!]]
-            [ajax.core :refer [GET]]
+  (:require [ajax.core :refer [GET]]
             [cljs.core.async :as async :refer [<! >! chan close! sliding-buffer put! alts!]])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
@@ -10,13 +9,19 @@
 (defn log [s]
   (js/console.log (clj->js s)))
 
+(defn ajax-channel [url]
+  (let [ch (chan)]
+    (GET url
+         {:handler #(go (>! ch %))})
+    ch))
+
 (defn- initialize! [window]
   (go
-    (let [manifest (<! (loadManifest!))
-          samples (get manifest "samples")
-          melodies (get manifest "melodies")]
-      (log samples)
-      (log melodies))))
+    (let [manifest (<! (ajax-channel "/manifest.json"))
+          melody-paths (map #(str "/melodies/" %) (get manifest "melodies"))
+          samples (get manifest "samples")]
+      (log (vector? (into [] (map ajax-channel melody-paths))))
+      (log samples))))
 
 ; (defonce initialized (do (initialize! js/window) true))
 (soon #(do (js/console.clear) (soon initialize!)))
