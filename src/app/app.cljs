1b0cebd51841928a0a6b3630ba14a3305c06feea
Ivan Reese <ivanreese@gmail.com>
2016-05-07 21:05:11 -0600

    Cleanup

--- src/app/app.cljs
+++ src/app/app.cljs
@@ -4,18 +4,11 @@
             [cljs.core.async :as async :refer [<! >! chan close! sliding-buffer put! alts!]])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
-(defonce state (atom nil))
-
 (defn soon [fn]
   (js/window.setTimeout fn 30))
 
 (defn log [s]
   (js/console.log (clj->js s)))
-
-(defn- saveAndLog [v]
-  (do
-   
-   (log @state)))
 
 (defn- initialize! [window]
   (go
@@ -24,8 +17,6 @@
           melodies (get manifest "melodies")]
       (log samples)
       (log melodies))))
-    
-        
 
 ; (defonce initialized (do (initialize! js/window) true))
 (soon #(do (js/console.clear) (soon initialize!)))
