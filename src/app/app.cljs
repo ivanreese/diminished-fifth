9c3e3299b2fd48ee1cdd3dd71bcb058b0ff85fbf
Ivan Reese <ivanreese@gmail.com>
2016-05-08 10:11:17 -0600

    Loading all melodies

--- src/app/app.cljs
+++ src/app/app.cljs
@@ -1,7 +1,7 @@
 (ns ^:figwheel-always app.app
   (:require [ajax.core :refer [GET]]
-            [cljs.core.async :as async :refer [<! >! chan close! sliding-buffer put! alts!]])
-  (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
+            [cljs.core.async :as async :refer [<! >! chan close!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn soon [fn]
   (js/window.setTimeout fn 30))
@@ -12,16 +12,17 @@
 (defn ajax-channel [url]
   (let [ch (chan)]
     (GET url
-         {:handler #(go (>! ch %))})
+         {:handler #(go (>! ch %) (close! ch))})
     ch))
 
 (defn- initialize! [window]
   (go
     (let [manifest (<! (ajax-channel "/manifest.json"))
+          sample-paths (map #(str "/samples/" %) (get manifest "samples"))
           melody-paths (map #(str "/melodies/" %) (get manifest "melodies"))
-          samples (get manifest "samples")]
-      (log (vector? (into [] (map ajax-channel melody-paths))))
-      (log samples))))
+          melodies (<! (async/into [] (async/merge (map ajax-channel melody-paths))))]
+      (log sample-paths)
+      (log melodies))))
 
 ; (defonce initialized (do (initialize! js/window) true))
 (soon #(do (js/console.clear) (soon initialize!)))
